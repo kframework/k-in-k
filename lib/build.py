1c526fb1ddb6b610cc8681d7e6b178f06a2a4de4
Nishant Rodrigues <nishantjr@gmail.com>
2018-09-20 17:32:46 -0500

    lib/build.py: Check that generated Kore files parse.
    (It does not. It currently uses a sort variable where it should a Sort. Note
    that kore.k does not indicate this. )

--- lib/build.py
+++ lib/build.py
@@ -9,31 +9,46 @@ import os.path
 
 def test_kfront_to_kore(proj, kdef, testfile):
     out = main.krun( output = kink.builddir(testfile + '.out')
-                      , input  = testfile + ''
-                      )
+                   , input  = testfile + ''
+                   )
     kore = kdef.build( outputs = kink.builddir(testfile + '.kore')
                      , rule = 'kore-from-config'
                      , inputs = out
                      )
-    main.check_actual_expected(os.path.basename(testfile), kore, testfile + '.expected')
+    kink.build( outputs = testfile + '.kore.parses'
+              , rule    = 'kore-parser'
+              , inputs  = kore
+              )
+    return main.check_actual_expected(os.path.basename(testfile), kore, testfile + '.expected')
 
 # Project Definition
 # ==================
 
 kink = KProject()
+
+# Building Kore & Kore Support
+# ----------------------------
+
+### Submodule init update
+
+# TODO: Figure out how to avoid calling `stack build` all the time.
+kink.rule( 'kore-parser'
+         , description = 'kore-parser'
+         , command     = 'stack build kore:exe:kore-parser && stack exec kore-parser $in > $out'
+         )
+kink.build(kink.extdir('kore', '.git'), 'git-submodule-init')
+
+# Converting Frontend Definitions
+# -------------------------------
+
 main = kink.kdefinition( 'kink'
                        , main = kink.tangle('kink.md', kink.tangleddir('kink/kink.k'))
                        , backend = 'java'
                        , alias = 'kink'
                        , kompile_flags = '-I .'
                        )
-
-# Converting Frontend Definitions
-# -------------------------------
-
 kink.rule( 'kore-from-config'
          , description = 'Extracting <kore> cell'
          , command = 'lib/kore-from-config $in $out'
          )
 test_kfront_to_kore(main, kink, 'foobar/t/foobar.kfront')
-
