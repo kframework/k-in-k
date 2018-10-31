775edfd35939d7e8aabc79f08b6cf77dd01652eb
Nishant Rodrigues <nishantjr@gmail.com>
2018-10-31 10:53:50 -0500

    t/foobar.ekore.expected: Make this a test

--- lib/build.py
+++ lib/build.py
@@ -54,6 +54,7 @@ def foobar_test(file):
 # foobar_test('t/foobar-backtick-synax.ekore')
 foobar_test('t/foobar-frontend-modules.ekore')
 foobar_test('t/foobar-declare-sorts.ekore')
+foobar_test('t/foobar.ekore.expected')
 
 # These tests are to make sure we can still parse IMP
 proj.source('imp/imp.ekore0').then(kink.krun()).default()
